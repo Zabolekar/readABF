93f9b73c5ade82d71baa4d7725b8900cf84b3e98
Zabolekar <syekirin@gmail.com>
2018-07-05 14:11:38 +0200

    removing tests from the repo because they need very specific files to run

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(abfReader)
-
-test_check("abfReader")
--- tests/testthat/test-malformed.R
+++
@@ -1,6 +0,0 @@
-context("Malformed files")
-
-test_that("We can't read malformed files", {
-   expect_error(readABF(examples("atfinfo.txt")), "unknown or incompatible file signature")
-   expect_error(readABF(examples("myconverter.m")), "unknown or incompatible file signature")
-})
